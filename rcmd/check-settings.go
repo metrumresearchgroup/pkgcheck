b54b8334b00305f40a9a490d011ce2d97589dab8
Devin Pastoor <devin.pastoor@gmail.com>
2018-03-21 10:38:58 -0400

    rename, implement Package
    Package method can be used to extract information about a package based
    on the provided tarball in CheckSettings, useful for
    folder construction or simple tasks where the name is useful

---
+++ rcmd/check-settings.go
@@ -0,0 +1,45 @@
+package rcmd
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+)
+
+// CmdFlags returns a string representation of the command flags associated with
+// CheckSettings
+func (cs CheckSettings) CmdFlags() []string {
+	// by default there is going to be some convention about
+	// how much to expose from R CMD check, as this tool has a different
+	// scope
+	output := []string{
+		"--no-manual",
+		"--no-build-vignettes",
+	}
+
+	if cs.Vanilla {
+		output = append(output, "--vanilla")
+	}
+	if cs.Cran {
+		output = append(output, "--as-cran")
+	}
+	if cs.OutputDir != "" {
+		output = append(output, fmt.Sprintf("--output=%s", cs.OutputDir))
+	}
+	return output
+}
+
+// Package returns information about the package information
+func (cs CheckSettings) Package() Package {
+	if cs.TarPath == "" {
+		return Package{}
+	}
+	tarball := filepath.Base(cs.TarPath)
+	tarball = strings.TrimSuffix(tarball, ".tar.gz")
+	// package tarball stored as <package>_<version>.tar.gz
+	packageVersion := strings.SplitN(tarball, "_", 2)
+	return Package{
+		Name:    packageVersion[0],
+		Version: packageVersion[1],
+	}
+}
