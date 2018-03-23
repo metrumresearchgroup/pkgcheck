8638c5ec92a6760ff3b7b4032a139bea9b9f2952
Devin Pastoor <devin.pastoor@gmail.com>
2018-03-22 20:52:19 -0400

    json struct tags

--- rcmd/structs.go
+++ rcmd/structs.go
@@ -2,21 +2,21 @@ package rcmd
 
 // Package stores information about the package
 type Package struct {
-	Name string
+	Name string `json:"name,omitempty"`
 	// this should eventually be some semver type or something hopefully
-	Version string
+	Version string `json:"version,omitempty"`
 }
 
 // RSettings controls settings related to managing libraries
 type RSettings struct {
-	LibPaths []string
-	Rpath    string
+	LibPaths []string `json:"lib_paths,omitempty"`
+	Rpath    string   `json:"rpath,omitempty"`
 }
 
 // CheckSettings defines settings related to R CMD CHECK
 type CheckSettings struct {
-	TarPath   string
-	OutputDir string
-	Vanilla   bool
-	Cran      bool
+	TarPath   string `json:"tar_path,omitempty"`
+	OutputDir string `json:"output_dir,omitempty"`
+	Vanilla   bool   `json:"vanilla,omitempty"`
+	Cran      bool   `json:"cran,omitempty"`
 }
