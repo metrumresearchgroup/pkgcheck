02c91c4a4635f0e07f7e525652d21cba976cdc5a
Devin Pastoor <devin.pastoor@gmail.com>
2018-04-10 13:02:26 -0400

    remove old r-pkgs folder as now in integration_test

--- r-pkgs/test1/R/my_median.R
+++
@@ -1,6 +0,0 @@
-#' calculate a median
-#' @param ... dots to pass to median
-#' @export
-my_median <- function(...) {
-  stats::median(...)
-}
--- r-pkgs/testdoc/R/my_median.R
+++
@@ -1,7 +0,0 @@
-#' calculate a median
-#' @param something param doesn't exist
-#' @param ... dots to pass to median
-#' @export
-my_median <- function(...) {
-  stats::median(...)
-}
