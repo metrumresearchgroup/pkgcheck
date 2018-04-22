4f49a7bd3052009516f8b1d6cf42f8ead67c871f
Devin Pastoor <devin.pastoor@gmail.com>
2018-04-22 19:00:19 -0400

    parse the check log

--- rcmdparser/parse.go
+++ rcmdparser/parse.go
@@ -1,5 +1,31 @@
 package rcmdparser
 
-func parse() {
+import (
+	"bytes"
+)
 
+// ParseCheckLog parses the check log
+func ParseCheckLog(e []byte) CheckResults {
+	splitOutput := bytes.Split(e, []byte("* "))
+	var errors []string
+	var notes []string
+	var warnings []string
+
+	for _, ent := range splitOutput {
+		switch {
+		case bytes.Contains(ent, []byte("... NOTE")):
+			notes = append(notes, string(ent))
+		case bytes.Contains(ent, []byte("... ERROR")):
+			errors = append(errors, string(ent))
+		case bytes.Contains(ent, []byte("... WARNING")):
+			warnings = append(warnings, string(ent))
+		default:
+			continue
+		}
+	}
+	return CheckResults{
+		Errors:   errors,
+		Notes:    notes,
+		Warnings: warnings,
+	}
 }
