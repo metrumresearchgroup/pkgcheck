a0b9816f6a0a3d8c151814724f3dfba7a4ff6327
Devin Pastoor <devin.pastoor@gmail.com>
2018-04-24 15:36:05 -0400

    NewCheck implemented

--- rcmdparser/check-output.go
+++ rcmdparser/check-output.go
@@ -1,6 +1,26 @@
 package rcmdparser
 
+import (
+	"github.com/spf13/afero"
+)
+
+// NewCheck creates a new CheckOutput Object
+// fs and check directory
+func NewCheck(fs afero.Fs, cd string) (LogResults, error) {
+	cr, err := ReadCheckDir(fs, cd)
+	if err != nil {
+		return LogResults{}, err
+	}
+	return cr.Parse(), nil
+}
+
 // Parse output to LogResults
 func (c CheckOutput) Parse() LogResults {
-	return LogResults{}
+	lr := LogResults{
+		Checks: ParseCheckLog(c.Check),
+	}
+	if c.Test.Testthat {
+		lr.Tests = ParseTestLog(c.Test.Results)
+	}
+	return lr
 }
