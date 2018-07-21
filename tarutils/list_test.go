373cce587babee8a245b86096ad44e0d8fbeed0b
Devin Pastoor <devin.pastoor@gmail.com>
2018-07-21 10:09:31 -0400

    benchmarks no longer needed
    results showed that inspecting the tarballs was unsurprisingly
    more computationally expensive, taking around 0.05 seconds per package
    to inspect the tarballs, vs < 0.0001
    seconds to parse the string
    hence the best strategy is to only
    inspect if doesn't have package naming.
    
    In either case, the entire operation takes < 1 second

--- tarutils/list_test.go
+++ tarutils/list_test.go
@@ -1,8 +1,6 @@
 package tarutils
 
 import (
-	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/r-infra/pkgcheck/rcmd"
@@ -61,35 +59,6 @@ func TestCopyTars(t *testing.T) {
 		for i, p := range actual {
 			if p != tt.expected[i] {
 				t.Errorf("GOT: %s, WANT: %s", actual, tt.expected)
-			}
-		}
-	}
-}
-
-func BenchmarkList(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		ListTars("/Users/devinp/Downloads/pkglock_snapshot/pkglib/packrat/src")
-	}
-}
-
-func BenchmarkListAndParseNaive(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		tars := ListTars("/Users/devinp/Downloads/pkglock_snapshot/pkglib/packrat/src")
-		for _, t := range tars {
-			PackageInfo(t)
-		}
-	}
-}
-func BenchmarkListAndParse(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		tars := ListTars("/Users/devinp/Downloads/pkglock_snapshot/pkglib/packrat/src")
-		for _, t := range tars {
-			tarball := filepath.Base(t)
-			tarball = strings.TrimSuffix(tarball, ".tar.gz")
-			// package tarball stored as <package>_<version>.tar.gz
-			packageVersion := strings.SplitN(tarball, "_", 2)
-			if len(packageVersion) < 2 {
-				PackageInfo(t)
 			}
 		}
 	}
