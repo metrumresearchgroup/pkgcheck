ffebd5895349f48feacb39b0ce716a1413c06157
Devin Pastoor <devin.pastoor@gmail.com>
2018-08-01 23:04:24 -0400

    first pass at handling hashed tarballs
    copy to tmp folder, untar, rename to package name, copy to destination

--- tarutils/list.go
+++ tarutils/list.go
@@ -2,9 +2,12 @@ package tarutils
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/mholt/archiver"
 
 	"github.com/dpastoor/goutils"
 	"github.com/r-infra/pkgcheck/rcmd"
@@ -49,25 +52,69 @@ func CopyPkgTars(fs afero.Fs, root string, dest string, fm rcmd.FilterMap) ([]st
 		tarball := filepath.Base(t)
 		tarball = strings.TrimSuffix(tarball, ".tar.gz")
 		// package tarball stored as <package>_<version>.tar.gz
-		var pm rcmd.Package
+		var pm PackageTarball
 		packageVersion := strings.SplitN(tarball, "_", 2)
 		if len(packageVersion) < 2 {
 			// if not a package this will come back as empty for name/version
-			pm = PackageInfo(t)
+			pm.Details = PackageInfo(t)
 		} else {
-			pm.Name = packageVersion[0]
-			pm.Version = packageVersion[1]
+			pm.Details.Name = packageVersion[0]
+			pm.Details.Version = packageVersion[1]
+			pm.PackratHashed = true
 		}
-		ok := rcmd.ShouldCheck(pm.Name, fm)
+		ok := rcmd.ShouldCheck(pm.Details.Name, fm)
 		if ok {
-			copyPath := filepath.Clean(filepath.Join(dest, fmt.Sprintf("%s_%s.tar.gz", pm.Name, pm.Version)))
-			_, err := goutils.CopyFS(fs, t, copyPath)
-			if err != nil {
-				// TODO: hmm don't know what the best thing to do logging wise
+			copyPath := filepath.Clean(filepath.Join(dest, fmt.Sprintf("%s_%s.tar.gz", pm.Details.Name, pm.Details.Version)))
+			if !pm.PackratHashed {
+				_, err := goutils.CopyFS(fs, t, copyPath)
+				if err != nil {
+					// TODO: hmm don't know what the best thing to do logging wise
+				}
+			} else {
+				ok, err := CopyPackratTarball(pm.Details, t, dest)
+				if !ok || err != nil {
+					// TODO: hmm don't know what the best thing to do logging wise
+				}
 			}
 			copied = append(copied, copyPath)
 		}
 	}
 
 	return copied, nil
+}
+
+// CopyPackratTarball sets up the tarball with the proper naming conventions in a temp directory
+// setup tarball takes the path of a tarball and returns a path of the new tarball or an error
+// the new tarball will have the proper folder name (the package name)
+func CopyPackratTarball(pm rcmd.Package, p string, dest string) (bool, error) {
+	dir, err := ioutil.TempDir("", pm.Name)
+	defer os.RemoveAll(dir) // clean up
+	if err != nil {
+		return false, err
+	}
+	err = archiver.TarGz.Open(p, dir)
+	if err != nil {
+		return false, err
+	}
+	tardir, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return false, err
+	}
+	newDir := filepath.Join(dir, pm.Name)
+	err = os.Rename(filepath.Join(
+		dir, tardir[0].Name()),
+		newDir)
+	if err != nil {
+		return false, err
+	}
+	newTar, err := os.Open(fmt.Sprintf("%s/%s_%s.tar.gz", dest, pm.Name, pm.Version))
+	if err != nil {
+		return false, err
+	}
+	err = archiver.TarGz.Write(newTar, []string{newDir})
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
